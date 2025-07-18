2513d10e68ab9eaeb276196c03a35ce59ab2c1de
unknown <JS2415@bijisen.local>
2025-07-18 12:09:24 +0900

    Book formの編集(カテゴリ)とバリデーションを追加しました

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
+  # 空でないことを確認するバリデーション
+  validates :title, :author, :isbn, :category, presence: true
+  # 整数かつ0以上であることを確認するバリデーション
+  validates :total_copies, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
