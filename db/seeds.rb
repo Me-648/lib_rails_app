6405d94de6ccd83ee3b051032b97b14a3fd383e8
unknown <JS2415@bijisen.local>
2025-09-29 10:02:19 +0900

    seedを編集&UIを編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,18 @@
 
 # bookサンプル
 Book.create!(title: "Rubyの本", author: "しもはし", isbn: "1234", category: "小説", total_copies: 123)
+Book.create!(title: "坪内学園の真実", author: "だいちゃんカンパニー株式会社", isbn: "17", category: "小説", total_copies: 1)
+Book.create!(title: "1945年8.9", author: "JUN", isbn: "1", category: "芸術", total_copies: 3)
 
 # Userサンプル
 User.create!(student_no: "12345", name: "山田太郎", role: 0, password: "pass")
 User.create!(student_no: "99999", name: "司書花子", role: 1, password: "admin")
+
+# 延滞サンプル
+Loan.create!(
+  user_id: user1.id,
+  book_id: book.id,
+  borrowed_at: 1.month.ago,
+  due_at: 10.days.ago,
+  returned_at: nil  # まだ返却していない
+)
